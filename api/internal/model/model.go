100870863e800fcb723fe1c489cb4bf82502b43a
Mohammad A. Apu <mohammadapu1@gmail.com>
2024-02-09 16:20:11 +0600

    Abstracted id, createdAt and updatedAt fields in baseModel

--- api/internal/model/model.go
+++ api/internal/model/model.go
@@ -5,12 +5,16 @@ import (
 	"time"
 )
 
-type User struct {
+type baseModel struct {
 	gorm.Model
 	ID        uint      `json:"id" gorm:"primaryKey"`
-	Name      string    `json:"name" gorm:"not null"`
-	Email     string    `json:"email" gorm:"not null"`
-	Password  string    `json:"password" gorm:"not null"`
 	CreatedAt time.Time `json:"created_at" gorm:"not null"`
 	UpdatedAt time.Time `json:"updated_at" gorm:"not null"`
+}
+
+type User struct {
+	baseModel
+	Name     string `json:"name" gorm:"not null"`
+	Email    string `json:"email" gorm:"not null"`
+	Password string `json:"password" gorm:"not null"`
 }
