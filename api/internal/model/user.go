e95044c31e2f8e83028dacf4289058ed173fd61b
Mohammad A. Apu <mohammadapu1@gmail.com>
2024-02-12 02:33:41 +0600

    Customized User model DB schema

--- api/internal/model/user.go
+++ api/internal/model/user.go
@@ -5,10 +5,10 @@ import "time"
 type User struct {
 	ID uint `json:"id" gorm:"primaryKey"`
 
-	Name     string `json:"name" gorm:"not null"`
-	Email    string `json:"email" gorm:"not null"`
-	Password string `json:"password" gorm:"not null"`
+	Name     string `json:"name" gorm:"type:varchar(30); not null"`
+	Email    string `json:"email" gorm:"type:varchar(40); not null"`
+	Password string `json:"password" gorm:"type:varchar(100); not null"`
 
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	CreatedAt time.Time `json:"created_at" gorm:"type:datetime"`
+	UpdatedAt time.Time `json:"updated_at" gorm:"type:datetime"`
 }
