6726e68a0628208fba9d65f98c61590e4568afe2
Ariful Hoque Al Amin <muhammadarif9416@yahoo.com>
2024-02-16 13:25:31 +0600

    add: separate forms

--- web/src/components/containers/auth/login-register-container.tsx
+++ web/src/components/containers/auth/login-register-container.tsx
@@ -1,20 +1,20 @@
 import { createSignal } from 'solid-js';
 import { Button } from '~/components/ui/button';
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '~/components/ui/card';
 import { Dialog, DialogContent, DialogTrigger } from '~/components/ui/dialog';
-import { Input } from '~/components/ui/input';
-import { Label } from '~/components/ui/label';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '~/components/ui/tabs';
-import { TextInput } from '~/components/ui/text-input';
+
+import LoginForm from './login';
+import RegisterForm from './register';
 
 const LoginRegisterContainer = () => {
   const [openModal, setOpenModal] = createSignal(false);
 
   return (
     <Dialog open={openModal()} onOpenChange={setOpenModal}>
-      <DialogTrigger>
-        <Button>Login</Button>
-      </DialogTrigger>
+      {/* <DialogTrigger>
+      
+      </DialogTrigger> */}
+      <Button onClick={() => setOpenModal(true)}>Login</Button>
       <DialogContent class="min-h-[475px]">
         <Tabs defaultValue="login" class="px-5 h-full">
           <TabsList class="grid w-full grid-cols-2">
@@ -23,52 +23,11 @@ const LoginRegisterContainer = () => {
           </TabsList>
 
           <TabsContent value="login" class="mt-8">
-            <Card>
-              <CardHeader>
-                <CardTitle>Login</CardTitle>
-              </CardHeader>
-              <CardContent class="space-y-2">
-                <div class="space-y-1">
-                  {/* <Label for="email">Email</Label>
-                  <Input id="email" /> */}
-                  <TextInput name="email" label="Email" error="" />
-                </div>
-                <div class="space-y-1">
-                  <Label for="password">password</Label>
-                  <Input id="password" type="password" />
-                </div>
-              </CardContent>
-              <CardFooter>
-                <Button>Login</Button>
-              </CardFooter>
-            </Card>
+            <LoginForm />
           </TabsContent>
 
           <TabsContent value="register">
-            <Card>
-              <CardHeader>
-                <CardTitle>Create Your Account</CardTitle>
-              </CardHeader>
-              <CardContent class="space-y-2">
-                <div class="space-y-1">
-                  {/* <Label for="name">Name</Label> */}
-                  {/* <Input id="name" /> */}
-                  <TextInput name="name" label="Name" error="" />
-                </div>
-
-                <div class="space-y-1">
-                  <Label for="email">Email</Label>
-                  <Input id="email" />
-                </div>
-                <div class="space-y-1">
-                  <Label for="password">password</Label>
-                  <Input id="password" type="password" />
-                </div>
-              </CardContent>
-              <CardFooter>
-                <Button>Create Account</Button>
-              </CardFooter>
-            </Card>
+            <RegisterForm />
           </TabsContent>
         </Tabs>
       </DialogContent>
--- web/src/components/containers/auth/login.tsx
+++ web/src/components/containers/auth/login.tsx
@@ -1,4 +1,23 @@
-const Login = () => {
-  return <div>Login</div>;
+import { Button } from '~/components/ui/button';
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '~/components/ui/card';
+import { Input } from '~/components/ui/input';
+import { Label } from '~/components/ui/label';
+import { TextInput } from '~/components/ui/text-input';
+
+const LoginForm = () => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Login</CardTitle>
+      </CardHeader>
+      <CardContent class="space-y-2">
+        <TextInput name="email" label="Email" error="" />
+        <TextInput name="password" label="Password" error="" type="password" />
+      </CardContent>
+      <CardFooter>
+        <Button type="submit">Login</Button>
+      </CardFooter>
+    </Card>
+  );
 };
-export default Login;
+export default LoginForm;
